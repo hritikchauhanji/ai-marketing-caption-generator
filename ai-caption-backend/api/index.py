6bb6f9830b5a2f4f41c600a350728bd647317cbf
Hritik Chauhan <hritikchji@gmail.com>
2025-11-26 11:13:27 +0530

    update index fiie

--- ai-caption-backend/api/index.py
+++ ai-caption-backend/api/index.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import google.generativeai as genai
+from fastapi.responses import JSONResponse 
 
 load_dotenv()
 
