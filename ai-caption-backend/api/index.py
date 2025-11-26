002f3a9d2b10760f712c9e394d3e0358d1870649
Hritik Chauhan <hritikchji@gmail.com>
2025-11-26 11:06:19 +0530

    udpate backend for deployment

--- ai-caption-backend/api/index.py
+++ ai-caption-backend/api/index.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[os.getenv("FRONTEND_URL")], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -19,6 +19,9 @@ app.add_middleware(
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
+@app.get("/")
+def home():
+    return JSONResponse({"message": "FastAPI running on Vercel!"})
 
 @app.post("/generate-caption/")
 async def generate_caption(request: Request):
